880b59c9dbe4c326965573a9a29c7933c5f336a1
Mercy811 <xinyiye811@gmail.com>
2020-04-30 12:45:59 +0800

    [yxy] reshape data

---
+++ app/src/main/python/reshape.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def utils_gen_windows(data_length, window_size, step_width):
+    "Generate indices for window-slizes with given length & step width."
+    start = 0
+    while start < data_length:
+        yield start, start + window_size
+        start += step_width
+
+def reshape(raw_data, window_size, step_width):
+    '''
+    input: 
+        [n_input,9]
+            n_input = n_sec * frequence
+            9, data from 3 sensors
+    
+    output: 
+        [n_output, window_size, 9]
+            n_output = 
+    '''
+    new_feat_count = ((raw_data.shape[0] - window_size) // step_width) + 1
+    reshaped_feat = np.empty((new_feat_count, window_size, 9))
+    for idx, window in enumerate(
+        utils_gen_windows(raw_data.shape[0], window_size, step_width)
+    ):
+        new_row = raw_data[window[0] : window[1]]
+        if idx < new_feat_count:
+            reshaped_feat[idx, :] = new_row
+                
+    return reshaped_feat
