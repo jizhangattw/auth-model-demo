c649e88058c116a62360b2d7b08f3c77b0514b59
yumei.hou <840972208@qq.com>
2020-05-07 15:17:10 +0800

    [yumei] add optimized scale data function

--- app/src/main/python/scale_data.py
+++ app/src/main/python/scale_data.py
@@ -2,12 +2,32 @@ from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import java
 
-def scale_data(raw_data):
+
+def _scale_data(X, data_min_=None, data_max_=None, save_scaler=False):
+    scaler = MinMaxScaler()
+    if data_min_ is not None:
+        scaler.fit_transform(np.array([data_min_, data_max_]))
+        scaled_data = scaler.transform(X)
+    else:
+        scaled_data = scaler.fit_transform(X)
+        if save_scaler:
+            np.save("scaler.npy", np.vstack((scaler.data_min_, scaler.data_max_)))
+
+    if np.all(scaled_data >= 0) and np.all(scaled_data <= 1):
+        return scaled_data
+    else:
+        return None
+
+
+def scale_data(raw_data, load_scaler=False, save_scaler=False):
     '''
     :param raw_data: 2D array if before reshape [n,9]  if after gen feature [n, 64]
-    :return: scaled data by column
+    :return: scaled data by column or None
     '''
-    scaler = MinMaxScaler()
     python_raw_data = java.cast(java.jarray(java.jarray(java.jfloat)), raw_data)
-    X = scaler.fit_transform(np.array(python_raw_data))
-    return X
+    X = np.array(python_raw_data)
+    if load_scaler:
+        scaler_min_max_array = np.load("scaler.npy")
+        return _scale_data(X, data_min_=scaler_min_max_array[0], data_max_=scaler_min_max_array[1])
+    else:
+        return _scale_data(X, save_scaler=save_scaler)
